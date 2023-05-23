e810caebeeb1e823cceebaba9ea875d8c01fae61
amna-alhashmi <amnaalhashmi98@gmail.com>
2023-05-23 10:27:49 +0400

    @AutoWired for slackClient

--- src/main/java/com/codeline/firstSpringDemo/SchedulJob/SchedulSchool.java
+++ src/main/java/com/codeline/firstSpringDemo/SchedulJob/SchedulSchool.java
@@ -1,4 +1,11 @@
 package com.codeline.firstSpringDemo.SchedulJob;
 
+import com.codeline.firstSpringDemo.Slack.SlackClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class SchedulSchool {
+    @Autowired
+    SlackClient slackClient;
 }
