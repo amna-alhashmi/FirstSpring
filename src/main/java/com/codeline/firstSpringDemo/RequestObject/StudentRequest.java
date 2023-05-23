8bd5096ee4ced615bc1bf15cb07291206d4b9c33
amna-alhashmi <amnaalhashmi98@gmail.com>
2023-05-23 09:41:14 +0400

    @Data

--- src/main/java/com/codeline/firstSpringDemo/RequestObject/StudentRequest.java
+++ src/main/java/com/codeline/firstSpringDemo/RequestObject/StudentRequest.java
@@ -1,10 +1,12 @@
 package com.codeline.firstSpringDemo.RequestObject;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Data
 public class StudentRequest {
     String FirstName;
     String LastName;
