3a32b5773bc751bcc38b99db979b1386e5d2e138
amna-alhashmi <amnaalhashmi98@gmail.com>
2023-05-23 10:34:26 +0400

    add cron for getAllSchools

--- src/main/java/com/codeline/firstSpringDemo/SchedulJob/SchedulSchool.java
+++ src/main/java/com/codeline/firstSpringDemo/SchedulJob/SchedulSchool.java
@@ -1,9 +1,14 @@
 package com.codeline.firstSpringDemo.SchedulJob;
 
+import com.codeline.firstSpringDemo.Models.Course;
+import com.codeline.firstSpringDemo.Models.School;
 import com.codeline.firstSpringDemo.Services.SchoolService;
 import com.codeline.firstSpringDemo.Slack.SlackClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class SchedulSchool {
@@ -11,4 +16,15 @@ public class SchedulSchool {
     SlackClient slackClient;
     @Autowired
     SchoolService schoolService;
+    @Scheduled(cron ="0 */15 * * * *")
+    public List<School> getAllSchools(){
+        List<School> school=schoolService.getAllSchools();
+        slackClient.sendMessage("This is for school");
+        for (School s:school)
+        {
+            slackClient.sendMessage("the id is :"+s.getId().toString()+"the name of student is:"+s.getName());
+        }
+        return school;
+    }
+
 }
